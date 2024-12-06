530ca64fbea286411d49ad0e0a922ab96e5d2be2
kooqooo <koogmlcks@gmail.com>
2024-12-06 22:15:53 +0900

    feat: 문단 수 분포 추가

--- data/eda.py
+++ data/eda.py
@@ -57,12 +57,12 @@ if __name__ == '__main__':
 
     from matplotlib import pyplot as plt
 
-    df = pd.read_csv('data/data.csv')
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data.csv'))
     
     # 데이터 개수
     print(f"\n데이터 개수 : {len(df):,}")
 
-    # 단어수 분포
+    # 단어 수 분포
     df['word_count'] = df['essay'].apply(lambda x: len(x.split()))
     print(f"\n총 단어 수 : {df['word_count'].sum():,}")
     print(f"평균 단어 수 : {df['word_count'].mean()}")
@@ -72,7 +72,7 @@ if __name__ == '__main__':
     plt.title('Word Count Distribution')
     plt.show()
 
-    # 문장수 분포
+    # 문장 수 분포
     df['sentence_count'] = df['essay'].apply(lambda x: x.count('.'))
     print(f"\n총 문장 수 : {df['sentence_count'].sum():,}")
     print(f"평균 문장 수 : {df['sentence_count'].mean()}")
@@ -80,6 +80,12 @@ if __name__ == '__main__':
     print(f"최소 문장 수 : {df['sentence_count'].min()}")
     plt.hist(df['sentence_count'], bins=100)
     plt.title('Sentence Count Distribution')
+    plt.show()
+
+    # 문단 수 분포
+    counter = {1: 37863, 2: 783, 3: 435, 4: 254, 5: 119, 6: 51, 7: 35, 8: 14, 10: 11, 9: 9, 11: 4, 12: 4, 13: 3, 14: 3, 15: 2, 16: 1}
+    plt.bar(counter.keys(), counter.values())
+    plt.title('Paragraph Count Distribution')
     plt.show()
 
     # 고유 단어수
@@ -92,19 +98,19 @@ if __name__ == '__main__':
     """
     아래의 코드는 실행 시간이 오래 걸립니다.
     """
-    # 명사 수 분포
-    df['nouns'] = df['essay'].apply(extract_nouns)
-    df['noun_count'] = df['nouns'].apply(len)
-    print(f"명사 수 : {df['noun_count'].sum()}")
-    plt.hist(df['noun_count'], bins=50)
-    plt.title('Noun Count Distribution')
-    plt.show()
+    # # 명사 수 분포
+    # df['nouns'] = df['essay'].apply(extract_nouns)
+    # df['noun_count'] = df['nouns'].apply(len)
+    # print(f"명사 수 : {df['noun_count'].sum()}")
+    # plt.hist(df['noun_count'], bins=50)
+    # plt.title('Noun Count Distribution')
+    # plt.show()
 
-    # 고유 명사 수
-    proper_nouns = []
-    for essay in df['essay']:
-        proper_nouns += extract_proper_nouns(essay)
-    unique_proper_nouns = set(proper_nouns)
-    print(f"총 고유 명사 수 : {len(proper_nouns)}")
-    print(f"고유 명사 수 : {len(unique_proper_nouns)}")
+    # # 고유 명사 수
+    # proper_nouns = []
+    # for essay in df['essay']:
+    #     proper_nouns += extract_proper_nouns(essay)
+    # unique_proper_nouns = set(proper_nouns)
+    # print(f"총 고유 명사 수 : {len(proper_nouns)}")
+    # print(f"고유 명사 수 : {len(unique_proper_nouns)}")
 
